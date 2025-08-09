e2b4279648deebc8e1038de5e0df403c7c8fd2c9
erokhinms <erokhinms@bcs.ru>
2025-08-09 13:36:21 +0500

    Cache update handler

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module fariante_gv
 
 go 1.24.5
 
-require github.com/robfig/cron/v3 v3.0.1
+
--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"sync"
 	"time"
-
-	"github.com/robfig/cron/v3"
 )
 
 // Структура для кэша данных
@@ -17,55 +17,57 @@ type Cache struct {
 	mu   sync.RWMutex
 }
 
+type updResp struct {
+	UpdTm string `json:"updTm"`
+}
+
 // Глобальный кэш
 var cache = &Cache{}
 
 // URL 1С
 const oneCUrl = "https://1c.fariante.ru/acc/hs/catalog/all"
 
-func fetchDataFrom1C() {
+func fetchDataFrom1C() (string, error) {
+
 	client := &http.Client{Timeout: 10 * time.Second}
 	req, err := http.NewRequest("GET", oneCUrl, nil)
 	if err != nil {
-		log.Printf("Ошибка создания запроса к 1С: %v", err)
-		return
+		return "Ошибка создания запроса к 1С: ", err
 	}
 
 	pass := os.Getenv("PASS_1C")
 	if pass == "" {
-		log.Println("Переменная окружения PASS_1C не установлена")
-		return
+		return "Переменная окружения PASS_1C не установлена", err
 	}
 
 	req.SetBasicAuth("Интеграция", pass)
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Printf("Ошибка получения данных из 1С: %v", err)
-		return
+		return "Ошибка получения данных из 1С", err
 	}
+
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		log.Printf("1С вернул статус: %d", resp.StatusCode)
-		return
+		return "Ошибка", fmt.Errorf("статус %d", resp.StatusCode)
 	}
 
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Printf("Ошибка чтения данных из 1С: %v", err)
-		return
+		return "Неразборные данные в теле", err
 	}
 
 	cache.mu.Lock()
 	cache.data = data
 	cache.mu.Unlock()
 
-	log.Printf("Данные из 1С обновлены: %s", time.Now().Format(time.RFC3339))
+	return time.Now().Format(time.DateTime), nil
 }
 
 // Возврат кешированных данных
-func handleData(w http.ResponseWriter, r *http.Request) {
+func dataGetHandler(w http.ResponseWriter, r *http.Request) {
+
 	cache.mu.RLock()
 	data := cache.data
 	cache.mu.RUnlock()
@@ -75,30 +77,46 @@ func handleData(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Printf("Отправка данных из кэша: %s", time.Now().Format(time.RFC3339))
-
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Header().Set("Access-Control-Allow-Origin", "https://jacobbermudes.github.io")
+	w.Header().Set("Access-Control-Allow-Origin", "*") //исправить для прода
 	w.Write(data)
 }
 
+func dataUpdateHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, `{"error": "Метод не поддерживается"}`, http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*") //исправить для прода
+
+	updDate, err := fetchDataFrom1C()
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf(`{"error": "%s"}`, err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	timing := updResp{
+		UpdTm: updDate,
+	}
+
+	json.NewEncoder(w).Encode(timing)
+}
+
 func main() {
+
 	fetchDataFrom1C()
 
-	// Обновление данных из 1С каждый час
-	c := cron.New()
-	_, err := c.AddFunc("@hourly", fetchDataFrom1C)
-	if err != nil {
-		log.Fatalf("Ошибка настройки планировщика: %v", err)
-	}
-	c.Start()
-
-	// Настройка HTTP-сервера
-	http.HandleFunc("/1cgw/data", handleData)
+	http.HandleFunc("/1cgw/data", dataGetHandler)
+	http.HandleFunc("/1cgw/update", dataUpdateHandler)
 
 	port := ":3210"
 	log.Printf("Сервер запущен на порту %s", port)
-	if err := http.ListenAndServeTLS(port, "/etc/letsencrypt/live/fariante.ru/fullchain.pem", "/etc/letsencrypt/live/fariante.ru/privkey.pem", nil); err != nil {
+	//if err := http.ListenAndServeTLS(port, "/etc/letsencrypt/live/fariante.ru/fullchain.pem", "/etc/letsencrypt/live/fariante.ru/privkey.pem", nil); err != nil {
+	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatalf("Ошибка запуска сервера: %v", err)
 	}
 }
